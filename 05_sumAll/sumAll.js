52aa477ff5e0a6f6a64e98c71112844e4366b6ea
Kaylan Wang <kaylanwang5@gmail.com>
2023-06-16 21:17:07 -0400

    05 - 6 tests passed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(bot, top) {
+    if (typeof bot !== "number" || 
+        typeof top !== "number" ||
+        bot < 0 ||
+        top < 0){
+        return "ERROR"
+    }   
+    if (bot > top){
+        let temp = top
+        top = bot
+        bot = temp
+    }
+    let num = 0
+    for (i = bot; i <= top; i++){
+        num += i
+    }
+    return num
 };
 
 // Do not edit below this line
