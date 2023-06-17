5a08a55fa4c6eb434e12d9c25b27f266b828cd1c
Kaylan Wang <kaylanwang5@gmail.com>
2023-06-16 20:42:25 -0400

    03 - 4 tests passed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const array = string.split("")
+    const newArray = array.reverse()
+    const reversedStr = newArray.join("")
+    return reversedStr
 };
 
 // Do not edit below this line
