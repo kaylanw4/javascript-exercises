84676dd31dac717f65c5e6f611c2b43ba4c0cda0
Kaylan Wang <kaylanwang5@gmail.com>
2023-06-19 19:49:31 -0400

    08 - 16 tests passed

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+  return nums.reduce((total, current) => total + current, 0)
 };
 
 const multiply = function() {
-
+  let total = 1
+  for (const num of arguments) {
+    total *= num
+  }
+  return total
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a**b
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let factorial = a
+	for (let i = a - 1; i > 0; i--){
+    factorial *= i
+  }
+  return a === 0 || a === 1 ? 1 : factorial
 };
 
 // Do not edit below this line
