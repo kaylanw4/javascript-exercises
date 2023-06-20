bec10f3442bd567d4bb53b32020f60fb4871935d
Kaylan Wang <kaylanwang5@gmail.com>
2023-06-19 21:28:00 -0400

    09 - 8 tests passed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let newStr = string.match(/[^_\W]+/g).join('').toLowerCase();
+    let palindrome = newStr.split('').reverse().join("")
+    return newStr === palindrome ? true : false
 };
 
 // Do not edit below this line
