2561de270b163bb87d5a1a27cab2d60ceb16b3aa
Kaylan Wang <kaylanwang5@gmail.com>
2023-06-20 14:37:32 -0400

    10 - 9 tests passed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
+const fibonacci = function(a) {
+    let n1 = 0, n2 = 1, nextTerm
 
+    for(let i = 1; i <= a; i++){
+        nextTerm = n1 + n2
+        n1 = n2
+        n2 = nextTerm
+    }
+    return a < 1 ? "OOPS" : n1
 };
 
 // Do not edit below this line
